7deba759f078283520b2d6045bf675852601de11
Fuzhou Chen <fuzhouch@users.noreply.github.com>
2021-05-25 20:19:42 +0800

    [NEW] Enable prometheus integration (#1)
    This change enables a basic prometheus counter, registered in /metrics endpoint. Right now it does not support advanced configurations like skippers.

--- echo.go
+++ echo.go
@@ -1,15 +1,42 @@
 package barton
 
 import (
+	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	elog "github.com/labstack/gommon/log"
+	pg "github.com/prometheus/client_golang/prometheus"
 	"github.com/rs/zerolog/log"
 	"github.com/ziflex/lecho/v2"
 )
 
-// NewEcho creates an Echo engine attaching default Zerolog.
-func NewEcho() *echo.Echo {
+type echoAppConfig struct {
+	appName          string
+	enablePrometheus bool
+}
+
+// NewEchoBuilder is main entry to start building an Echo app engine.
+// It returns a chainable configuration object, echoAppConfig, which
+// is configured as setter functions. The final step is it calls New()
+// function to really build an Echo engine, plus a cleanup function
+// returned.
+func NewEchoBuilder() *echoAppConfig {
+	return &echoAppConfig{
+		appName: "Barton-Echo-App",
+	}
+}
+
+// AppName setter sets app name for Echo engine. By defualt the name is
+// set to Barton-Echo-App.
+func (c *echoAppConfig) AppName(name string) *echoAppConfig {
+	c.appName = name
+	return c
+}
+
+// New method creates a real echo.Echo object, plus a cleanup() function
+// to execute internal cleanup logic, such as unregistering Prometheus
+// metrics collector in global registry.
+func (c *echoAppConfig) New() (*echo.Echo, func()) {
 	e := echo.New()
 	wrapper := lecho.From(log.Logger)
 	e.Logger = wrapper
@@ -17,5 +44,13 @@ func NewEcho() *echo.Echo {
 	e.Use(middleware.Recover())
 
 	e.Logger.SetLevel(elog.INFO)
-	return e
+
+	p := prometheus.NewPrometheus(c.appName, nil)
+	p.Use(e)
+	cleanupFunc := func() {
+		for _, m := range p.MetricsList {
+			pg.Unregister(m.MetricCollector)
+		}
+	}
+	return e, cleanupFunc
 }
